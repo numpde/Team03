a6c61a35b2bba760898a36ec40ccf217214c706f
klugh <klugh@student.ethz.ch>
2020-09-30 13:47:32 +0200

    added get_matching_blocks function to smith_waterman.py

--- project1/solution/aligner03/align/smith_waterman.py
+++ project1/solution/aligner03/align/smith_waterman.py
@@ -42,20 +42,66 @@ class Smith_waterman_aligner(object):
         i_, j_ = np.unravel_index(H_flip.argmax(), H_flip.shape)
         i, j = np.subtract(H.shape, (i_ + 1, j_ + 1))  # (i, j) are **last** indexes of H.max()
         if H[i, j] == 0:
-            return query_, j
+            return query_, i
         query_ = self.query[j - 1] + '-' + query_ if old_i - i > 1 else self.query[j - 1] + query_
         return self.traceback(H[0:i, 0:j], query_, i)
 
     def get_start_end(self):
+        """
+        Get start and end index of query in reference. 0-based!
+        """
         # make reference and query uppercase
         self.ref, self.query = self.ref.upper(), self.query.upper()
         H = self.create_scoring_matrix()
-        b_, pos = self.traceback(H)
-        return pos, pos + len(b_)
+        query_, pos = self.traceback(H)
+        return pos, pos + len(query_)
+
+    def get_matching_blocks(self):
+        """
+        Get list of 1-based matching intervals
+        Example:
+            reference = ABCD
+            query = ABD
+            query_ = AB-D
+            return: [[1, 2], [4, 4]]
+        """
+        list = []
+        matching = False
+        last_item = False
+        self.ref, self.query = self.ref.upper(), self.query.upper()
+        H = self.create_scoring_matrix()
+        query_, pos = self.traceback(H)
+        if query_ == self.ref[pos:pos + len(query_)]:
+            # query_ matches exactly ref
+            list.append([pos + 1, pos + len(query_)])
+        else:
+            idx = 1
+            for a, b in zip(self.ref, query_):
+                if idx == len(query_):
+                    last_item = True
+                if a == b:
+                    if matching == False:
+                        start_idx = idx
+                        if last_item:
+                            # single last query_ item corresponds to ref
+                            list.append([pos + start_idx, pos + start_idx])
+                    elif last_item:
+                        list.append([pos + idx, pos + idx])
+                    matching = True
+                else:
+                    if matching:
+                        end_idx = idx - 1
+                        list.append([pos + start_idx, pos + end_idx])
+                    matching = False
+                # print(idx, a, b, matching)
+                idx += 1
+
+        return list
 
 
 if __name__ == '__main__':
-    test_waterman = Smith_waterman_aligner(ref='ATGGCCTC', query='ACGGCTC', gap_cost=4, match_score=1, mismatch_cost=3)
+    test_waterman = Smith_waterman_aligner(ref='ABCD', query='ABD', gap_cost=1, match_score=3, mismatch_cost=5)
     print(test_waterman.create_scoring_matrix())
     print(test_waterman.traceback(test_waterman.scoring_matrix))
     print(test_waterman.get_start_end())
+    print(test_waterman.get_matching_blocks())
