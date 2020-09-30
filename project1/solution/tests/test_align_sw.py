cb96633d850d023a85982cd1b58375b6fa01d4d2
klugh <klugh@student.ethz.ch>
2020-09-30 13:48:12 +0200

    added test for get_matching_blocks function

--- project1/solution/tests/test_align_sw.py
+++ project1/solution/tests/test_align_sw.py
@@ -3,7 +3,7 @@ from align import Smith_waterman_aligner
 
 def test_smith_waterman_aligner():
     test_waterman = Smith_waterman_aligner(ref='ATGGCCTC', query='ACGGCTC', gap_cost=4, match_score=1, mismatch_cost=3)
-    start, end = test_waterman.get_start_end()
+    matching_blocks = test_waterman.get_matching_blocks()
     # true values from http://rna.informatik.uni-freiburg.de/Teaching/index.jsp?toolName=Smith-Waterman#
     true_scoring_matrix = [[0, 0, 0, 0, 0, 0, 0, 0],
                            [0, 1, 0, 0, 0, 0, 0, 0],
@@ -14,4 +14,7 @@ def test_smith_waterman_aligner():
                            [0, 0, 1, 0, 0, 1, 0, 1],
                            [0, 0, 0, 0, 0, 0, 2, 0],
                            [0, 0, 1, 0, 0, 1, 0, 3]]
+    # true matching blocks calculated on paper
+    true_matching_blocks = [[1, 1], [3, 5]]
     assert (test_waterman.scoring_matrix == true_scoring_matrix).all
+    assert matching_blocks == true_matching_blocks
