800415f58815dc9ea15681f1f75bddf262e20f9f
RA <numpde@null.net>
2020-10-08 14:07:49 +0200

    FASTQ reader

--- project1/solution/aligner03/io/fq.py
+++ project1/solution/aligner03/io/fq.py
@@ -29,30 +29,36 @@ class Read:
 def from_fastq(file) -> typing.Iterable[Read]:
     """
     Yields FASTQ reads from file (filename or descriptor).
+    Note: seek(0) is called.
     """
 
-    def phred_as_ints(phred: str) -> typing.List[int]:
-        # TODO
-        phred = [None] * len(phred)
+    def phred_as_ints(phred_as_string: str) -> typing.List[int]:
+        # https://en.wikipedia.org/wiki/FASTQ_format
+        phred = [(ord(c) - 0x21) for c in phred_as_string]
+        assert (len(phred) == len(phred_as_string))
         return phred
 
-    if type(file) is str:
+    try:
+        file.seek(0)
+    except:
+        # Assume `file` needs to be opened first
         with open(file, mode="r") as file:
             yield from from_fastq(file)
-        return
-
-    while 1:
-        try:
-            SEP = '\t'
-            line = (lambda: file.readline().rstrip())
-            (at_name, desc) = ((line()).split(SEP, 1) + (None, ))[0:2]
-            seq = line()
-            line()
-            phred = line()
-
-            yield Read(at_name[1:].strip(), desc, seq, phred_as_ints(phred))
-        except:  # todo: appropriate exception
-            break
+            return
+    else:
+        while 1:
+            try:
+                SEP = '\t'
+                line = (lambda: file.readline().rstrip() or None)
+                (at_name, desc) = (line().split(SEP, 1) + [None])[0:2]
+                seq = line()
+                line()
+                phred = line()
+            except AttributeError:
+                # Assume that line().split failed
+                break
+            else:
+                yield Read(at_name[1:].strip(), desc, seq, phred_as_ints(phred))
 
 
 def test_reads():
