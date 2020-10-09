ee215c39708d1292152d08cff5d57c715e3240ad
RA <numpde@null.net>
2020-10-09 12:18:58 +0200

    rm orphaned folder

--- project1/solution/tests/data__orphaned/io/fasta_test.py
+++
@@ -1,4 +0,0 @@
-fasta=Fasta("tests\data\io\genome.chr22.5K.fa")
-with open("genome_correct.txt","w",encoding="utf-8") as file:
-    for i in fasta.yield_sequences(4):
-        file.write(str(i)+'\n')
