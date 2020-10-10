69aadc46275778d8ed4fb7d58c10e02166a58fed
RA <numpde@null.net>
2020-10-10 15:55:34 +0200

    Check: read pairs are ordered

--- project1/solution/tests/test_io_fa_pairwise.py
+++ project1/solution/tests/test_io_fa_pairwise.py
@@ -1,8 +1,12 @@
 # RA, 2020-10-10
 
+"""
+Check that the reads are in the same order
+in FASTQ files.
+"""
+
 from pathlib import Path
 from unittest import TestCase
-
 from aligner03.io import assert_order_consistency
 
 
@@ -12,6 +16,5 @@ class TestPairwise(TestCase):
         assert_order_consistency(file1, file2)
 
     def test_is_pairwise_data_large(self):
-        raise NotImplementedError
         (file1, file2) = sorted((Path(__file__).parent / "data_for_tests/data/").glob("*.fq"))
         assert_order_consistency(file1, file2)
