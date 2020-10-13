e0730538d6c7b321b5280fd3e8997d001772ddb1
RA <numpde@null.net>
2020-10-13 16:31:36 +0200

    Simplify using built-in driver; header test

--- project1/solution/tests/test_main_atkh.py
+++ project1/solution/tests/test_main_atkh.py
@@ -3,12 +3,13 @@
 from unittest import TestCase
 from pathlib import Path
 
-from humdum.io import from_fasta
 from humdum.main import AllTheKingsHorses
 from humdum.utils import relpath, unlist1
-from humdum.index import FmIndex as GenomeIndex
-from humdum.align import SmithWaterman as Aligner
-# from humdum.index import NaiveIndex as GenomeIndex
+
+from humdum.io import AlignedSegment
+
+from humdum.io import from_sam
+from pysam import AlignedSegment as pysam_AlignedSegment
 
 from itertools import count
 
@@ -17,25 +18,15 @@ class TestATKH(TestCase):
     def test_on_data_small(self):
         data_root = Path(__file__).parent / "data_for_tests"
         source_path = data_root / "data_small"
-        (file1, file2) = sorted(source_path.glob("*.fq"))
 
+        (read_file1, read_file2) = sorted(source_path.glob("*.fq"))
         genome_file = unlist1(source_path.glob("genome*.fa"))
-        ref_genome = unlist1(from_fasta(genome_file)).seq
-        index = GenomeIndex(ref_genome)
 
-        aligner = Aligner()
-
-        atkh = AllTheKingsHorses(genome_index=index, sequence_aligner=aligner, ref_genome=ref_genome)
-
-        from humdum.io import AlignedSegment
-        from pysam import AlignedSegment as pysam_AlignedSegment
-        from humdum.io import from_sam
-
-        from_aether = atkh.map_paired(file1, file2)
+        aligned_segments = AllTheKingsHorses.from_files(fa=genome_file, fq1=read_file1, fq2=read_file2)
 
         mine: AlignedSegment
         theirs: pysam_AlignedSegment
-        for ((mine, theirs), n) in zip(zip(from_aether, from_sam(unlist1(source_path.glob("*.sam")))), count()):
+        for ((mine, theirs), n) in zip(zip(aligned_segments, from_sam(unlist1(source_path.glob("*.sam")))), count()):
             # See io/sam.py for the explanations
             self.assertEqual(mine.flag.is_minus_strand, bool(theirs.flag & 16))
             self.assertEqual(mine.flag.is_secondary_alignment, bool(theirs.flag & 256))
@@ -53,4 +44,8 @@ class TestATKH(TestCase):
                 print(F"Mine:  ", mine.cigar, "at", mine.pos)
                 print(F"Theirs:", theirs.cigarstring, "at", adjust_pysam_index(theirs.pos))
                 print(F"Read:  ", mine.seq)
-                print(F"Neighborhood:  ", ref_genome[(mine.pos - 10):(mine.pos + 10 + len(mine.seq))])
+                #print(F"Neighborhood:  ", atkh.ref_genome[(mine.pos - 10):(mine.pos + 10 + len(mine.seq))])
+
+
+    def test_header(self):
+        AllTheKingsHorses.header()
