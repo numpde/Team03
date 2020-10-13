55b95cc09c2c4e73deb41f1b225c76749cb685dd
RA <numpde@null.net>
2020-10-13 16:39:46 +0200

    Functional instructions

---
+++ project1/solution/bin/humdum_aligner.py
@@ -0,0 +1,46 @@
+# RA, 2020-10-13
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+from humdum.main import AllTheKingsHorses
+
+
+def get_args():
+    parser = ArgumentParser(description="Align reads to a reference genome.")
+
+    parser.add_argument(
+        "fasta", type=str, nargs=1, help="Reference genome as FASTA file."
+    )
+
+    parser.add_argument(
+        "fastq", type=str, nargs=2, help="Two FASTQ files."
+    )
+
+    args = parser.parse_args()
+
+    def assert_exists(file: Path):
+        assert file.exists(), F"Oh, file `{file}`, where art thou?"
+        return file
+
+    return {
+        'fa': assert_exists(Path(args.fasta[0])),
+        'fq1': assert_exists(Path(args.fastq[0])),
+        'fq2': assert_exists(Path(args.fastq[1])),
+    }
+
+
+def main():
+    files = get_args()
+
+    try:
+        print(AllTheKingsHorses.header())
+    except NotImplementedError:
+        print("@HEADER")
+
+    for alignment in AllTheKingsHorses.from_files(**files):
+        print(alignment)
+
+
+if __name__ == '__main__':
+    main()
