666aa9b033bf7de45dad078ba22a0f8643004677
RA <numpde@null.net>
2020-10-13 17:34:47 +0200

    Template

--- project1/solution/tests/test_index_fm_bigfile.py
+++ project1/solution/tests/test_index_fm_bigfile.py
@@ -4,7 +4,8 @@ from unittest import TestCase
 from pathlib import Path
 
 from humdum.io import open_maybe_gz
-from humdum.utils import unlist1
+from humdum.io import from_fasta
+from humdum.utils import unlist1, first
 
 data_root = Path(__file__).parent / "data_for_tests/data"
 genome_file = unlist1(data_root.glob("*.fa.gz"))
@@ -13,10 +14,11 @@ genome_file = unlist1(data_root.glob("*.fa.gz"))
 class TestFm(TestCase):
     def test_open_and_read(self):
         with open_maybe_gz(genome_file) as fd:
-            print(fd.readline())
-            print(fd.readline())
-            print(fd.readline())
+            (fd.readline())
+            (fd.readline())
+            (fd.readline())
 
     def test_index(self):
-        # TODO
+        genome = first(from_fasta(genome_file))
+        print(len(genome))
         raise NotImplementedError
