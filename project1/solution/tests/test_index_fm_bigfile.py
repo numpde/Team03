9938d93eb7584e2b6dedb1ec56c375a2599879f1
lblum <lblum@student.ethz.ch>
2020-10-18 17:20:08 +0200

    read big genomea

--- project1/solution/tests/test_index_fm_bigfile.py
+++ project1/solution/tests/test_index_fm_bigfile.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from humdum.io import open_maybe_gz
 from humdum.io import from_fasta
 from humdum.utils import unlist1, first
+from humdum.index import FmIndex as GenomeIndex
 
 data_root = Path(__file__).parent / "data_for_tests/data"
 genome_file = unlist1(data_root.glob("*.fa.gz"))
@@ -14,11 +15,27 @@ genome_file = unlist1(data_root.glob("*.fa.gz"))
 class TestFm(TestCase):
     def test_open_and_read(self):
         with open_maybe_gz(genome_file) as fd:
-            (fd.readline())
-            (fd.readline())
-            (fd.readline())
+            fd.readline()
+            fd.readline()
+            fd.readline()
 
     def test_index(self):
-        genome = first(from_fasta(genome_file))
-        print(len(genome))
-        raise NotImplementedError
+
+        genome = ""
+
+        with open_maybe_gz(genome_file) as fd:
+
+            #skip first line
+            line = fd.readline()
+            line = fd.readline().rstrip()
+            while True:
+
+                genome += line
+                line = fd.readline().rstrip()
+                if not line:
+                    break
+        print("lenght", len(genome))
+
+        index = GenomeIndex(genome)
+
+        print(index.F)
