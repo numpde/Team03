53ca0ff047fe4af706710d28f813c2320d0c9174
RA <numpde@null.net>
2020-10-23 21:01:42 +0200

    more tests

--- project1/solution/tests/test_io_fa.py
+++ project1/solution/tests/test_io_fa.py
@@ -2,13 +2,31 @@
 
 import unittest
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 from humdum.io import from_fasta, Sequence
+from humdum.utils import first
 
 data_root = Path(__file__).parent / "data_for_tests"
 
 
 class TestFastaReader(unittest.TestCase):
+    def test_reads_well(self):
+        desc = "Hello"
+        seq1 = "ABC"
+        seq2 = "DEF"
+        with NamedTemporaryFile(mode='w') as fn:
+            print(*[">" + desc, seq1, seq2], sep='\n', file=fn, flush=True)
+            record = first(from_fasta(fn.name))
+            self.assertEqual(record.desc, desc)
+            self.assertEqual(record.seq, seq1 + seq2)
+
+    def test_fails_when_many(self):
+        with NamedTemporaryFile(mode='w') as fn:
+            print(*[">A", "N", ">B", "N"], sep='\n', file=fn, flush=True)
+            with self.assertRaises(AssertionError):
+                list(from_fasta(fn.name))
+
     def test_on_data_small(self):
         source_path = data_root / "data_small"
         files = list(source_path.glob("*.fa"))
@@ -28,9 +46,10 @@ class TestFastaReader(unittest.TestCase):
 
     def test_on_data_big(self):
         source_path = data_root / "data"
-        files = source_path.glob("*.fa*")
+        files = list(source_path.glob("*.fa.gz"))
         assert files
 
         for file in files:
             for genome in from_fasta(file):
-                print(F"Got genome of length {len(genome.seq)}")
+                self.assertEqual(len(genome.seq), 51304566)
+                self.assertTrue(genome.seq.strip("N").endswith("CGGATT"))
