e927a093e42979da1c1b701cbe6b8510487ced6f
klugh <klugh@student.ethz.ch>
2020-11-13 21:49:53 +0100

    change str to pathlib.Path

--- project2/solution/tests/test_classify_variants.py
+++ project2/solution/tests/test_classify_variants.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 from idiva.bin.classify_variants import predict
+from pathlib import Path
 
 
 class TestClf(TestCase):
     def test_predict(self):
-        vcf_file_path = "tests/data_for_tests/large_head/case_processed.vcf"
+        vcf_file_path = Path("tests/data_for_tests/large_head/case_processed.vcf")
         which_clf = 'dummy'
-
         results = predict(vcf_file_path, which_clf)
         self.assertTrue(len(results))
