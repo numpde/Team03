274c395d725edc328fc42c70f1ebf14844c7c204
RA <numpde@null.net>
2020-11-13 16:12:47 +0100

    quickfire comments

--- project2/solution/idiva/main.py
+++ project2/solution/idiva/main.py
@@ -1,27 +1,49 @@
 import argparse
 
+# TODO: `utils` are things that are useful cross-project; refactor to `helpers` or better to `clf`
 from idiva.utils.clf_related import create_df, get_clf
 
+from pathlib import Path
 
-def main(args: any):
+
+# TODO: refact to `bin` folder, cf. project1
+# TODO: rename to `classify_patients`
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vcf_file', type=Path, help="VCF file to classify the variants")
+    parser.add_argument('--classifier', type=str, default='', help="Classifier to use for predictions")
+    flags = parser.parse_args()
+
+    assert isinstance(flags.vcf_file, Path)
+    assert flags.vcf_file.is_file()
+
+    flags.classifier
+    # TODO: check validity
+
+    return dict(vcf=flags.vcf_file, classifier=flags.classifier)
+
+
+def predict(vcf: Path, classifier=None):
     """
     Reads a given vcf file, transforms it into a dataframe and uses a pretrained classifier to classify each variant in
     the vcf with 0 for control and 1 for "sick". It then adds the label to the input vcf and returns it (or saves it).
     """
-    df = create_df(args.vcf_file_path)
-    clf = get_clf(args)
-    input_data = df.to_numpy()
-    prediction = clf.predict(input_data)
 
-    # todo: somehow include the predictions into the input vcf file
+    assert vcf.is_file()
+
+    df = create_df(vcf)
+    clf = get_clf(classifier)
+    # TODO: Don't bother with numpy arrays unless there is a good reason -- ?
+    prediction = clf.predict(df)
+
     return prediction
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('vcf_file_path', type=str, help='vcf-file that contains the variants to be classified')
-    parser.add_argument('--which_clf', type=str, default='',
-                        help="indicates which classifier is used to make the predictions")
+def main():
+    prediction = predict(**parse_args())
+    # todo: somehow include the predictions into the input vcf file
 
-    flags = parser.parse_args()
-    main(flags)
+
+if __name__ == '__main__':
+    main()
