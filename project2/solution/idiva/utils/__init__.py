e49caa34772a108eb16955acc92628107218c4a3
RA <numpde@null.net>
2020-11-15 00:35:28 +0100

    seek0 context manager

--- project2/solution/idiva/utils/__init__.py
+++ project2/solution/idiva/utils/__init__.py
@@ -3,3 +3,4 @@ from .strings import reverse, forward
 from .paths import relpath, assert_exists
 from .containers import unlist1, minidict, first, at_most_n
 from .plox import Plox
+from .files import seek0
