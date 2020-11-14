2c7aad584e8a05ab16a2e2bcc0dd99765ba325ad
RA <numpde@null.net>
2020-11-15 00:35:23 +0100

    seek0 context manager

--- project2/solution/idiva/utils/files.py
+++ project2/solution/idiva/utils/files.py
@@ -1,6 +1,12 @@
 # RA, 2020-10-19
 
+import typing
 import contextlib
 
-# @contextlib.contextmanager
-# def open_file():
+
+@contextlib.contextmanager
+def seek0(fd: typing.IO):
+    pos = fd.tell()
+    fd.seek(0)
+    yield fd
+    fd.seek(pos)
