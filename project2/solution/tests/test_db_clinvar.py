790cf2965e9d9e5fc3aae2d1503d159e546ee4a7
RA <numpde@null.net>
2020-11-14 19:58:34 +0100

    clinvar howto

--- project2/solution/tests/test_db_clinvar.py
+++ project2/solution/tests/test_db_clinvar.py
@@ -8,6 +8,15 @@ from idiva.utils import at_most_n
 
 
 class TestClinVar(TestCase):
+    def test_howto(self):
+        from idiva.db import clinvar_open
+        from idiva.io.vcf import ReadVCF
+        with clinvar_open() as fd:
+            vcf = ReadVCF(fd)
+            vcf.meta
+            for dataline in vcf:
+                dataline
+
     def test_import(self):
         from idiva.db import clinvar_open
 
@@ -31,6 +40,12 @@ class TestClinVar(TestCase):
             self.assertListEqual(reference, candidate)
 
     def test_open_read_vcf_meta(self):
+        from idiva.db import clinvar_open
+        from idiva.io import ReadVCF
+        with clinvar_open(which='vcf_37') as fd:
+            vcf = ReadVCF(fd)
+            assert not hasattr(vcf, "sample_ids")
+            print(vcf.header)
         raise NotImplementedError
 
     def test_open_read_vcf_datalines(self):
@@ -38,7 +53,6 @@ class TestClinVar(TestCase):
         from idiva.io import ReadVCF
         with clinvar_open(which='vcf_37') as fd:
             vcf = ReadVCF(fd)
-            assert not hasattr(vcf, "sample_ids")
 
             reference = [
                 "1	865568	846933	G	A	.	.	ALLELEID=824438;CLNDISDB=MedGen:CN517202;CLNDN=not_provided;CLNHGVS=NC_000001.10:g.865568G>A;CLNREVSTAT=criteria_provided,_single_submitter;CLNSIG=Uncertain_significance;CLNVC=single_nucleotide_variant;CLNVCSO=SO:0001483;GENEINFO=SAMD11:148398;MC=SO:0001583|missense_variant;ORIGIN=1",
@@ -48,7 +62,7 @@ class TestClinVar(TestCase):
 
             from idiva.io.vcf import RawDataline
             datalines: typing.List[RawDataline]
-            datalines = list(at_most_n(vcf.datalines, n=len(reference)))
+            datalines = list(at_most_n(vcf, n=len(reference)))
 
             self.assertIsInstance(datalines[0], RawDataline)
 
