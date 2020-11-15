7df5601eab858aa7b43ca6550af030aab47896d2
RA <numpde@null.net>
2020-11-15 12:39:42 +0100

    test on small and large vcf

--- project2/solution/tests/test_io_vcf_alignvcf.py
+++ project2/solution/tests/test_io_vcf_alignvcf.py
@@ -1,5 +1,7 @@
 # RA, 2020-11-14
 
+import pandas as pd
+import numpy as np
 import typing
 import io
 
@@ -7,6 +9,17 @@ from unittest import TestCase
 from pathlib import Path
 from idiva.utils import unlist1, at_most_n, first
 from tcga.utils import download
+
+# LARGE DATA EXTRACT
+
+BASE = (Path(__file__).parent) / "data_for_tests/large_head"
+
+PATHS = {
+    'ctrl': BASE / "control.vcf",
+    'case': BASE / "case_processed.vcf",
+}
+
+# LARGE DATA
 
 download_cache = (Path(__file__).parent.parent.parent / "input/download_cache")
 assert download_cache.is_dir()
@@ -17,23 +30,25 @@ URLS = {
     'case': "https://public.bmi.inf.ethz.ch/eth_intern/teaching/cbm_2020/cbm_2020_project2/case_processed.vcf",
 }
 
-# UNFINISHED
+
+# TESTS
 
 class TestAlignVCF(TestCase):
-    def test_align(self):
-        from idiva.io.vcf import ReadVCF, RawDataline
-        from idiva.io.vcf import AlignVCF
+    def test_align_largehead(self):
+        from idiva.io.vcf import ReadVCF
+        from idiva.io.vcf import align
+
+        with PATHS['case'].open() as fd_case:
+            with PATHS['ctrl'].open() as fd_ctrl:
+                df = align(case=ReadVCF(fd_case), ctrl=ReadVCF(fd_ctrl))
+                print(df)
+
+    def test_align_large(self):
+        from idiva.io.vcf import ReadVCF
+        from idiva.io.vcf import align
 
         data = {k: download(URLS[k]).now for k in URLS}
-        with data['ctrl'].open() as fd1, data['case'].open() as fd2:
-            vcf1 = ReadVCF(fd1)
-            vcf2 = ReadVCF(fd2)
-            for (n, (d1, d2)) in enumerate(AlignVCF(vcf1, vcf2), start=1):
-                print(n)
-                print(d1)
-                print(d2)
-
-                self.assertEqual(d1.pos, d2.pos)
-                self.assertEqual(d1.id, d2.id)
-
-
+        with data['case'].open() as fd_case:
+            with data['ctrl'].open() as fd_ctrl:
+                df = align(case=ReadVCF(fd_case), ctrl=ReadVCF(fd_ctrl))
+                print(df)
