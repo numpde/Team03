b23cec5ee9ff1395551e2a5e2d1b7f837702dca7
klugh <klugh@student.ethz.ch>
2020-11-15 18:05:49 +0100

    added get_info_dict for clinvar to df

--- project2/solution/idiva/db/clinvar.py
+++ project2/solution/idiva/db/clinvar.py
@@ -4,7 +4,13 @@ import contextlib
 import gzip
 import io
 import typing
+
+import pandas as pd
+
 import idiva.utils
+from idiva.io.vcf import ReadVCF
+from idiva.utils import at_most_n
+from tqdm import tqdm
 
 URL = {
     'vcf_37': "https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz",
@@ -27,6 +33,33 @@ def clinvar_meta(which='vcf_37') -> idiva.utils.minidict:
     return idiva.utils.minidict(data.meta)
 
 
+def get_info_dict(info) -> dict:
+    info_dict = {}
+    for elem in info.split(';'):
+        k, v = elem.split('=')
+        info_dict[k] = v
+    return info_dict
+
+
+def clinvar_to_df(output_path, which='vcf_37', make_checkpoints=False) -> pd.DataFrame:
+    """
+    Creates a dataframe from the clinvar file. Adds all the INFO fields as additional columns.
+    """
+    df = pd.DataFrame()
+    with clinvar_open(which=which) as fd:
+        vcf = ReadVCF(fd)
+        for idx, line in tqdm(enumerate(vcf.datalines), postfix='reading clinvar file'):
+            info_dict = get_info_dict(line.info)
+            line_dict = line.__dict__
+            del line_dict['info']
+            line_dict = dict(line_dict, **info_dict)
+            df = df.append(line_dict, ignore_index=True)
+            if idx % 500 == 0 and make_checkpoints:
+                df.to_csv(output_path, compression='gzip', index=False)
+    df.to_csv(output_path, compression='gzip', index=False)
+    return df
+
+
 def clinvar_rs_ids(which='vcf_37'):
     from idiva.io.vcf import ReadVCF
     with clinvar_open(which) as fd:
@@ -35,9 +68,6 @@ def clinvar_rs_ids(which='vcf_37'):
 
 
 if __name__ == '__main__':
-    from idiva.io.vcf import ReadVCF
-    from idiva.utils import at_most_n
-
     with clinvar_open('vcf_37') as fd:
         reader = ReadVCF(fd)
         print(*at_most_n(reader.datalines, n=10), sep='\n')
