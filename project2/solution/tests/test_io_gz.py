2b7cf46d776af06ed18e52aaa24d550d5c1ad85c
RA <numpde@null.net>
2020-11-18 20:48:31 +0100

    tests for open_maybe_gz

---
+++ project2/solution/tests/test_io_gz.py
@@ -0,0 +1,50 @@
+# RA, 2020-10-13
+
+import io
+
+from unittest import TestCase
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+from idiva.io import open_maybe_gz
+
+data_root = Path(__file__).parent / "data_for_tests/gz"
+
+ref_file = data_root / "test.txt"
+ref_file_gz = data_root / "test.txt.gz"
+
+with open(ref_file, mode='r') as fd:
+    reference = fd.read()
+
+
+class TestGz(TestCase):
+    def test_plain(self):
+        with open_maybe_gz(ref_file, mode='r') as fd:
+            self.assertEqual(reference, fd.read())
+
+    def test_gz(self):
+        with open_maybe_gz(ref_file_gz, mode='r') as fd:
+            candidate = fd.read()
+            self.assertIsInstance(candidate, str)
+
+        self.assertEqual(reference, candidate)
+
+    def test_open_rb(self):
+        with open_maybe_gz(ref_file, mode='rb') as fd:
+            self.assertEqual(reference, io.TextIOWrapper(fd).read())
+
+    def test_dont_open_r_as_rb(self):
+        with open(ref_file, mode='r') as fd:
+            with self.assertRaises(AssertionError):
+                with open_maybe_gz(fd, mode='rb'):
+                    pass
+
+    def test_open_from_rb_as_r(self):
+        with open(ref_file, mode='rb') as fd:
+            with open_maybe_gz(fd, mode='r') as fd:
+                self.assertEqual(reference, fd.read())
+
+    def test_open_from_rb_as_rb(self):
+        with open(ref_file, mode='rb') as fd:
+            with open_maybe_gz(fd, mode='rb') as fd:
+                self.assertEqual(reference, io.TextIOWrapper(fd).read())
