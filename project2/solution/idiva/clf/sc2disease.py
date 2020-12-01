a436ce8015f13327700bbec04560c30bb40d75b1
RA <numpde@null.net>
2020-12-01 11:25:03 +0100

    sc2disease draft

--- project2/solution/idiva/clf/sc2disease.py
+++ project2/solution/idiva/clf/sc2disease.py
@@ -8,9 +8,15 @@ URL = "http://easybioai.com/sc2disease/static/allgwas.txt"
 def allgwas() -> pandas.DataFrame:
     from idiva.download import download
     with download(URL).now.open() as fd:
-        return pandas.read_csv(fd, sep='\t', header=None)
-
+        return pandas.read_csv(fd, sep='\t', names=["gene", "rs", "p", "chrom", "pos", "disease"])
 
 
 if __name__ == '__main__':
-    print(allgwas())
+    from collections import Counter
+    allgwas = allgwas()
+
+    # https://www.snpedia.com/index.php/Rs7903146
+    (rs, __) = max(Counter(allgwas.rs).items(), key=(lambda x: x[1]))
+    assert rs == "rs7903146"
+    print(allgwas[allgwas.rs == rs])
+
