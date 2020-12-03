28c7865a43f3ccb72e8768d56b314254d5027180
RA <numpde@null.net>
2020-12-03 01:02:34 +0100

    rename test

--- project2/solution/tests/test_bin_classify_variants_predict.py
+++ project2/solution/tests/test_bin_classify_variants_predict.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 class TestClf(TestCase):
-    def test_predict(self):
+    def test_predict_does_something(self):
         vcf_file_path = Path("tests/data_for_tests/large_head/case_processed.vcf")
         which_clf = 'dummy'
         results = predict(vcf_file_path, which_clf)
