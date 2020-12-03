c7f604bd52095b88dd5963131dfc1e0d28cf1d71
RA <numpde@null.net>
2020-12-03 01:11:50 +0100

    cleanup

--- project2/solution/idiva/clf/df.py
+++ project2/solution/idiva/clf/df.py
@@ -1,22 +1,25 @@
-import typing
-from pathlib import Path
+from idiva import log
 
-import pandas
-import pandas as pd
-
-import numpy
 import idiva.io
-from idiva.db.clinvar import df_clinvar_to_clf_data
-from joblib import Parallel, delayed
 
-dtype_v0 = {'CHROM': str, 'POS': int, 'ID': str, 'ALT0': float, 'ALT1': float, 'ALT2': float}
+import typing
+import pandas
+import numpy
+
+# Do not remove:
+import joblib
+import tqdm
+
+
+def apply_dtype(df: pandas.DataFrame) -> pandas.DataFrame:
+    dtype_v0 = {'CHROM': str, 'POS': int, 'ID': str, 'ALT0': float, 'ALT1': float, 'ALT2': float}
+    return df.astype({c: dtype_v0[c] for c in df.columns if c in dtype_v0})
 
 
 def join(*, case: pandas.DataFrame, ctrl: pandas.DataFrame) -> pandas.DataFrame:
     """
     Outer-join two dataframes on the columns CHROM, POS, ID.
     Use the suffixes _case and _ctrl for the other ambiguous columns.
-    Uses sort=False in pandas.merge_ordered.
 
     RA, 2020-11-14
     """
@@ -34,28 +37,64 @@ def join(*, case: pandas.DataFrame, ctrl: pandas.DataFrame) -> pandas.DataFrame:
 def v0_datalines(vcf: idiva.io.ReadVCF) -> typing.Iterable[dict]:
     """
     RA, 2020-11-14
+    RA, 2020-12-01
     """
     from idiva.io.vcf import parse_gt
 
     # special = sorted({F"<{k}>" for k in vcf.meta['ALT'].keys()})
 
-    for dataline in vcf:
-        samples = [parse_gt(gt) for gt in dataline.samples]
+    def parse(dataline):
+        samples = tuple(parse_gt(gt) for gt in dataline.samples)
 
         # SIMPLIFICATION
 
-        samples = (numpy.array(samples, dtype=int) != 0).sum(axis=1)
+        samples = tuple(((a != 0) + (b != 0)) for (a, b) in samples)
+        # samples = (numpy.array(samples) != 0).sum(axis=1)
 
         line = {
             'CHROM': dataline.chrom,
             'POS': dataline.pos,
             'ID': dataline.id,
-            'ALT0': (samples == 0).sum(),
-            'ALT1': (samples == 1).sum(),
-            'ALT2': (samples == 2).sum(),
+            'ALT0': sum((x == 0) for x in samples),
+            'ALT1': sum((x == 1) for x in samples),
+            'ALT2': sum((x == 2) for x in samples),
         }
 
-        yield line
+        return line
+
+    log.debug("Parsing datalines.")
+
+    from tqdm import tqdm
+    from joblib import Parallel, delayed
+
+    lines = Parallel(n_jobs=8, prefer="processes")(
+        delayed(parse)(dataline)
+        for dataline in tqdm(vcf)
+    )
+
+    yield from lines
+
+
+def c3_df(vcf: idiva.io.ReadVCF) -> pandas.DataFrame:
+    """
+    Returns a dataframe like
+
+           CHROM   POS           ID
+        0     17    52  rs556541063
+        1     17    56  rs145615430
+        2     17    78  rs148170422
+        ..   ...   ...          ...
+
+    Rewinds the `vcf`.
+
+    RA, 2020-12-03
+    """
+
+    with vcf.rewind_when_done:
+        return apply_dtype(pandas.DataFrame(
+            data=((dataline.chrom, dataline.pos, dataline.id) for dataline in vcf),
+            columns=["CHROM", "POS", "ID"],
+        ))
 
 
 def v0_df(vcf: idiva.io.ReadVCF) -> pandas.DataFrame:
@@ -64,23 +103,27 @@ def v0_df(vcf: idiva.io.ReadVCF) -> pandas.DataFrame:
     Only counts the presence of alternative variants.
     Returns a dataframe like
 
-        ID          CHROM   POS  ALT0  ALT1  ALT2
-        rs556541063    17    52   500     0     0
-        ...           ...   ...   ...   ...   ...
-        rs191439637    17  2833   495     5     0
-        rs149757548    17  2837   498     2     0
+           CHROM   POS           ID   ALT0  ALT1  ALT2
+        0     17    52  rs556541063  314.0   0.0   0.0
+        1     17    56  rs145615430  314.0   0.0   0.0
+        2     17    78  rs148170422  313.0   1.0   0.0
+        ..   ...   ...          ...    ...   ...   ...
 
     ID is not guaranteed to be unique.
 
     `int` data type is used for the ALT columns (see `dtype_v0`).
 
+    Rewinds the `vcf`.
+
     RA, 2020-11-14
     """
 
-    return pandas.DataFrame(data=v0_datalines(vcf)).astype(dtype_v0)
+    with vcf.rewind_when_done:
+        log.debug("Getting the datalines and creating the dataframe.")
+        return apply_dtype(pandas.DataFrame(data=v0_datalines(vcf)))
 
 
-def get_clinvar_clf_data(base_string_encoding: str = 'integer') -> pd.DataFrame:
+def get_clinvar_clf_data(base_string_encoding: str = 'integer') -> pandas.DataFrame:
     """
     Loads clinvar_clf_data suitable for a classifier.
 
@@ -92,7 +135,7 @@ def get_clinvar_clf_data(base_string_encoding: str = 'integer') -> pd.DataFrame:
 
     which = 'vcf_37'
 
-    def maker_clinvar() -> pd.DataFrame:
+    def maker_clinvar() -> pandas.DataFrame:
         from idiva.db import clinvar_open
         from idiva.io import ReadVCF
         from idiva.db.clinvar import clinvar_to_df
@@ -102,7 +145,8 @@ def get_clinvar_clf_data(base_string_encoding: str = 'integer') -> pd.DataFrame:
 
     df_clinvar = cache_df(name=("clinvar_" + which), key=[], df_maker=maker_clinvar)
 
-    def maker_clinvar_clf() -> pd.DataFrame:
+    def maker_clinvar_clf() -> pandas.DataFrame:
+        from idiva.db.clinvar import df_clinvar_to_clf_data
         # If you change this function, change the cache key also.
         # Preparing the clinvar dataframe for categorical classification:
         df_clinvar_reduced = df_clinvar[df_clinvar['CLNSIG'].isin({'Pathogenic', 'Benign'})]
@@ -118,7 +162,6 @@ if __name__ == '__main__':
     }
 
     from tcga.utils import download
-    from idiva.utils import at_most_n
     from pathlib import Path
     from contexttimer import Timer
 
