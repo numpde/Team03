c87439ea1063effab7c9c371370c2d847c6f94e8
RA <numpde@null.net>
2020-12-03 01:03:57 +0100

    allow seek=None

--- project2/solution/idiva/utils/files.py
+++ project2/solution/idiva/utils/files.py
@@ -1,13 +1,17 @@
 # RA, 2020-10-19
 
+from idiva import log
+
 import typing
 import contextlib
+import io
 
 
 @contextlib.contextmanager
-def seek_then_rewind(fd: typing.IO, seek=0) -> typing.IO:
+def seek_then_rewind(fd: io.IOBase, seek=0) -> typing.IO:
     pos = fd.tell()
-    fd.seek(seek)
+    if seek is not None:
+        fd.seek(seek)
     try:
         yield fd
     finally:
