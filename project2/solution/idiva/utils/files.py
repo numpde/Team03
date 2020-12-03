96831fa4fdbe2539c91d956b0e2b9745cf884094
RA <numpde@null.net>
2020-12-03 01:04:27 +0100

    md5 sum

--- project2/solution/idiva/utils/files.py
+++ project2/solution/idiva/utils/files.py
@@ -16,3 +16,20 @@ def seek_then_rewind(fd: io.IOBase, seek=0) -> typing.IO:
         yield fd
     finally:
         fd.seek(pos)
+
+
+# Adapted from
+# https://stackoverflow.com/a/4213255
+def checksum_md5(fd: io.TextIOBase) -> str:
+    log.info("Computing md5 hash of stream.")
+    import hashlib
+    with seek_then_rewind(fd):
+        md5 = hashlib.md5()
+        try:
+            for chunk in iter(lambda: fd.read(128 * md5.block_size).encode(), b''):
+                md5.update(chunk)
+        except KeyboardInterrupt:
+            pass
+    md5 = md5.hexdigest()
+    log.debug(F"Computed md5 = {md5}.")
+    return md5
