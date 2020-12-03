33a2d8f7abaae5cd789fc48c9dce7463adda1f5e
RA <numpde@null.net>
2020-12-03 01:01:13 +0100

    cleanup

--- project2/solution/tests/test_clf_df_v0_largehead.py
+++ project2/solution/tests/test_clf_df_v0_largehead.py
@@ -1,6 +1,7 @@
 # RA, 2020-11-14
 
-import pandas
+import io
+import pandas as pd
 
 from unittest import TestCase
 from pathlib import Path
@@ -24,6 +25,7 @@ class TestDf(TestCase):
 
         for k in PATHS:
             with PATHS[k].open(mode='r') as fd:
+                assert isinstance(fd, io.TextIOBase)
                 with seek_then_rewind(fd):
                     datalines = list(ReadVCF(fd))
                 with seek_then_rewind(fd):
@@ -39,25 +41,39 @@ class TestDf(TestCase):
 
         for k in PATHS:
             with PATHS[k].open(mode='r') as fd:
+                assert isinstance(fd, io.TextIOBase)
                 dfs[k] = v0_df(ReadVCF(fd))
 
         candidate = join(case=dfs['case'], ctrl=dfs['ctrl'])
 
+        def read_csv(file) -> pd.DataFrame:
+            return pd.read_csv(file, sep=SEP).astype(
+                {
+                    'CHROM': str,
+                    'POS': int,
+                    'ID': str,
+                    'ALT0_case': float, 'ALT1_case': float, 'ALT2_case': float,
+                    'ALT0_ctrl': float, 'ALT1_ctrl': float, 'ALT2_ctrl': float,
+                }
+            )
+
         ref_file = MY_SPACE / "reference.txt"
-        # candidate.to_csv(ref_file, sep=SEP, index=False)
-        reference = pandas.read_csv(ref_file, sep=SEP).astype(
-            {
-                'CHROM': str,
-                'POS': int,
-                'ID': str,
-                'ALT0_case': float, 'ALT1_case': float, 'ALT2_case': float,
-                'ALT0_ctrl': float, 'ALT1_ctrl': float, 'ALT2_ctrl': float,
-            }
-        )
+
+        # Hack to create the reference
+        # candidate.to_csv(ref_file, sep=SEP, index=True)
+
+        reference = read_csv(ref_file)
+
+        # Write and read `candidate` to make comparable to `reference`
+        import tempfile
+        with tempfile.NamedTemporaryFile(mode='w') as tf:
+            candidate.to_csv(tf, sep=SEP, index=True)
+            tf.flush()
+            candidate = read_csv(tf.name)
 
         self.assertTrue(reference.equals(candidate))
 
-    def test_combine_and_show(self):
+    def test_join_does_something(self):
         from idiva.io import ReadVCF
         from idiva.clf.df import v0_df, join
 
@@ -65,8 +81,9 @@ class TestDf(TestCase):
 
         for k in PATHS:
             with PATHS[k].open(mode='r') as fd:
+                assert isinstance(fd, io.TextIOBase)
                 dfs[k] = v0_df(ReadVCF(fd))
 
         df = join(case=dfs['case'], ctrl=dfs['ctrl'])
 
-        print(df)
+        # print(df)
