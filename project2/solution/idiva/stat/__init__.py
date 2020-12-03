45d5838e4edec0749e5c5c901f1fc24f38ecf11a
RA <numpde@null.net>
2020-12-03 01:04:54 +0100

    silence warning

--- project2/solution/idiva/stat/__init__.py
+++ project2/solution/idiva/stat/__init__.py
@@ -1,2 +1,10 @@
+# RA, 2020-12-02
+
+# RuntimeWarning: numpy.ufunc size changed
+# https://github.com/numpy/numpy/issues/14920#issuecomment-554672523
+import warnings
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
+from .fisher import v0_fisher
 from .chi2 import chi2_test
 from .fisher import v0_fisher
