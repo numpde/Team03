f57c7bb5f80147d2d69131ba95b3e8ed569ab177
klugh <klugh@student.ethz.ch>
2020-12-06 18:26:23 +0100

    cleanup

--- project2/solution/tests/test_db_clf.py
+++ project2/solution/tests/test_db_clf.py
@@ -5,10 +5,8 @@ from unittest import TestCase
 
 from tcga.utils import download
 
-from idiva.clf.df import join, c5_df
-from idiva.db.db_clf import classify
+from idiva.db.db_clf import db_classifier
 from idiva.download import download
-from idiva.io import cache_df
 from idiva.io.gz import open_maybe_gz
 from idiva.io.vcf import ReadVCF
 
@@ -18,21 +16,9 @@ URLS = {
 }
 
 
-def case_control_maker():
-    dfs = {}
-    for group, value in URLS.items():
-        with download(value).now.open(mode='rb') as fd:
-            with open_maybe_gz(fd, mode='r') as fd:
-                dfs[group] = c5_df(ReadVCF(fd))
-
-    return join(case=dfs['case'], ctrl=dfs['ctrl'], on=['CHROM', 'POS', 'REF', 'ALT', 'ID'])
-
-
 class TestDbClf(TestCase):
     def test_db_clf(self):
         with download(URLS['ctrl']).now.open(mode='rb') as ctrl, download(URLS['case']).now.open(mode='rb') as case:
             with open_maybe_gz(ctrl, mode='r') as ctrl, open_maybe_gz(case, mode='r') as case:
-                result = classify(case=ReadVCF(case), ctrl=ReadVCF(ctrl),
-                                  case_control=cache_df(name=("case_control"), key='c5_df',
-                                                        df_maker=case_control_maker))
+                result = db_classifier(case=ReadVCF(case), ctrl=ReadVCF(ctrl))
         self.assertTrue(len(result.df))
